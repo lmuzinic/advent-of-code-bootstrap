5983fe42f7bc7cf0ddb200ea23deaf278c088bc2
Luka Muzinic <luka@muzinic.net>
2017-11-28 12:46:55 +0100

    Adds Day interface for puzzles

---
+++ src/Day.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+
+namespace AdventOfCode;
+
+
+interface Day
+{
+    public function firstPuzzle(string $input);
+    public function secondPuzzle(string $input);
+}
